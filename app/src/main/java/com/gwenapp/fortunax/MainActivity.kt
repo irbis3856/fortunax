84a0664a2afb5e707b802c90f3d83da1285fec37
irbis3856 <irbis3856@users.noreply.github.com>
2025-10-02 12:46:34 +0200

    play around with layouts and ui elements

--- app/src/main/java/com/gwenapp/fortunax/MainActivity.kt
+++ app/src/main/java/com/gwenapp/fortunax/MainActivity.kt
@@ -3,13 +3,18 @@ package com.gwenapp.fortunax
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gwenapp.fortunax.ui.theme.FortunaxTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +24,29 @@ class MainActivity : ComponentActivity() {
             FortunaxTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Row() {
+                            Text(
+                                text = "Hello1 World!",
+                                fontSize = 30.sp
+                            )
+                            Button(onClick = {}) {
+
+                            }
+                        }
+                        Spacer(modifier = Modifier.weight(10.0f))
+                        Row() {
+                            Text(
+                                text = "Hello2 World!",
+                                fontSize = 30.sp
+                            )
+                            Button(onClick = {}) {
+
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FortunaxTheme {
-        Greeting("Android")
     }
 }
